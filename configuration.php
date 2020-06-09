d1e894a141b755a458c3290a97c5ee101e8377e4
S-Thibault <sylvain.thibault@infamail.com>
2020-06-09 16:51:42 +0200

    Update configuration.php

--- configuration.php
+++ configuration.php
@@ -1,36 +1,18 @@
 ﻿<?php
 // Entrer le non du repertoire à explorer exemple:  'documents'  ou:  'doc/public/abcde'
-$data = 'partage';
+$data = 'C:\wamp64\www\ThibaultS';
 
 // Entrer le non du repertoire ou la zip serra créé. il faut donner les droits en écriture
-$destination = 'zip';
+$destination = 'C:\wamp64\www\ThibaultS';
 
-<?php
-
-if(isset($_POST['supprimer'])){
- // Le bouton supprimer a été cliqué
- $fichier_a_supprimer=$_POST['liste_fichiers'];
- if( file_exists ( "nouveau dossier/$fichier_a_supprimer")){
-  $effacer=unlink("nouveau dossier/$fichier_a_supprimer");
-  if($effacer){
-   echo "Le fichier ".$fichier_a_supprimer." a bien été supprimé !";
-  }
-  else
-  {
-  echo "Le fichier ".$fichier_a_supprimer." n'a pas pu être supprimé !";
-  }
-  }
-  else
-  {
-  echo "Fichier ".$fichier_a_supprimer." non trouvé !!";
- }
-}
 ?>
+
 <form action='' method='post' name='Form'>
 <table width='100%' border='0' cellspacing='1' cellpadding='1'>
 <label>Fichier</label> :  <select name="liste_fichiers">
+
 <?php
-$dirname = 'nouveau dossier';
+$dirname = 'C:\wamp64\www\ThibaultS';
 $dir = opendir($dirname);
 
 $array_liste_fichiers=array();
@@ -46,9 +28,11 @@ for($i=0;$i<sizeof($array_liste_fichiers);$i++){
 echo '<option value="'.$array_liste_fichiers[$i].'">'.$array_liste_fichiers[$i].'</option>';
 }
 
-?>
 <input type="submit" name="supprimer" value="Supprimer" >
 </table>
+?>
+
+<?php
 
 
 // NE PAS MODIFIER APRES CETTE LIGNE
